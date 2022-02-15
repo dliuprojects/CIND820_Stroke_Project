b9f74e2986bcbf9a2eeedd730c224c33dacc9d28
dliuprojects <diane.liu77@gmail.com>
2022-02-14 23:56:48 -0500

    Update stroke risk factors.R

--- stroke risk factors.R
+++ stroke risk factors.R
@@ -6,7 +6,19 @@
 # Course: CIND820
 # -------------------------------
 
-stroke <- read.csv(file="stroke.csv",header=T,
-                           sep=",")
+# Read dataset
+stroke <- read.csv(file="stroke.csv",header=T, sep=",")
+
+# Descriptive analysis
 str(stroke)
 summary(stroke)
+
+# Convert 'N/A's (strings) in dataset to NA
+is.na(stroke) <- stroke == "N/A"
+# Count number of NAs in dataset
+sum(is.na(stroke))
+# Count number of NAs in all columns
+colSums(is.na(stroke))
+
+# Count number of 'Unknown's in all columns
+colSums(stroke == "Unknown")

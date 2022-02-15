aea0315249290b6029d195e5204c32e2338b8c9a
dliuprojects <diane.liu77@gmail.com>
2022-02-14 23:08:37 -0500

    Source Code

---
+++ stroke risk factors.R
@@ -0,0 +1,12 @@
+# -------------------------------
+# Title: Stroke Factors
+# Author: Danyang Liu
+# Student no: 500936348
+# Supervisor: Ceni Babaoglu
+# Course: CIND820
+# -------------------------------
+
+stroke <- read.csv(file="stroke.csv",header=T,
+                           sep=",")
+str(stroke)
+summary(stroke)
